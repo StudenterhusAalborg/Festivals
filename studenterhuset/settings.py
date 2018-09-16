a708fb5491a81a75d36772ce94729a2aadf43d6d
Casper Bang <a@thecdk.net>
2018-09-16 19:30:23 +0200

    tweak whitenoise

--- studenterhuset/settings.py
+++ studenterhuset/settings.py
@@ -152,8 +152,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# commenting this out makes it crash.
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
