96d00a4e206a9546c8df86b7dcaf2ab9b09a332c
Casper Bang <a@thecdk.net>
2018-09-16 19:11:47 +0200

    use dj instead

--- studenterhuset/settings.py
+++ studenterhuset/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+
+import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -114,10 +116,7 @@ WSGI_APPLICATION = 'studenterhuset.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
